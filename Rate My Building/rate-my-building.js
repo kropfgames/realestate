4340bf7b08ecf09348205733b69cbbf6bd114034
Alex Kropf <kropovalex@gmail.com>
2020-03-10 22:34:43 -0700

    added an input field to the popups on the map

--- Rate My Building/rate-my-building.js
+++ Rate My Building/rate-my-building.js
@@ -14,7 +14,6 @@ var circle = L.circle([49.2835, -123.1153], {
 }).addTo(neighbourhoodMap);
 
 //DRAWS FIRST POPUP AT YOUR LOCATION
-
 var stars;
 stars = "☆☆☆☆☆";
 
@@ -56,64 +55,18 @@ function onStarClickLoggedOut()
   //popupAtYourLocation.update();
 }
 
-var signUpWithEmail;
-//should be a text field with a placeholder
-signUpWithEmail = " <br>Enter your email.";
+//an input field that allows the user to sign up with a credential
+var signUpInputField;
+<!--makes an sign up input field with a label -->
+signUpInputField = '<form id="signUpForm"><label for="signUpInputField">Sign up with your phone number</label><br><input id="signUpInputField" type="text" placeholder="1 604 456 7890"><br><input id="submitButtonText" type="submit" value="Send me a code"></form>';
 
 //listens for a click on a popup to enable signing up
 //the alert works but the popup doesn't update
 rateThisBuildingButton.addEventListener ("click", function() {
   //alert("did something");
-  noOneHasRatedClickToRateText = " 😢<br>Click here to rate. <br>" + stars + signUpWithEmail;
-  rateThisBuildingButton.innerHTML = "No one has rated this building!" + noOneHasRatedClickToRateText;
+  noOneHasRatedClickToRateText = stars + "<br>" + signUpInputField;
+  rateThisBuildingButton.innerHTML = noOneHasRatedClickToRateText;
   popupAtYourLocation.setContent(rateThisBuildingButton);
   popupAtYourLocation.update();
   popupAtASpot.update().setContent(rateThisBuildingButton);
 });
-
-
-//⭐
-
-//working on 
-/*
-var markerAtYourLocation = L.marker([49.2835, -123.1153]).bindPopup('No one has rated your building! <br>' + rateButton).addTo(neighbourhoodMap);
-markerAtYourLocation.openPopup();
-*/
-
-//rateButton.addEventListener ("click", function() {
-//  alert("did something");
-//});
-
-//need to add a button saying Rate now
-//must make "Rate now" clickable
-//on mouse click on Rate Now, open a picture of the house, as well as 5 stars
-
-/*
-//changing the content on mouseover
-markerAtYourLocation.on('mouseover', function(){
-    //make a label "Sign Up with Email"
-    //must make an input selector of stars
-    //markerAtYourLocation._popup.setContent('*****');
-    //on click of stars, open popup to enter email
-  //must add an input field for their email with placeholder text - Sign up with Email
-
-});
-*/
-
-
-
-
-
-
-
-
-//not working :( 
-//Makes a marker and adds button's inner HTML content
-//var markerAtYourLocation = L.marker([49.2835, -123.1153]).addTo(neighbourhoodMap);
-
-/*
-var popupAtYourLocation = L.popup()
-    .setLatLng([49.2835, -123.1153])
-    .setContent("No one has rated your building! <br>Rate now.")
-    .openOn(neighbourhoodMap);
-*/
